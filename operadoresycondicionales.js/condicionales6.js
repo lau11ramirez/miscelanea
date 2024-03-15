e770eeb0e52e43496757577962e3a67d59f7e95f
lau11ramirez <160690270+lau11ramirez@users.noreply.github.com>
2024-03-15 15:34:23 -0500

    Update condicionales6.js

--- operadoresycondicionales.js/condicionales6.js
+++ operadoresycondicionales.js/condicionales6.js
@@ -1,19 +1,19 @@
 console.log("Escriba dos numeros")
 
-let A = -20
-let B = 2
+let a = -20
+let e = 2
 
 console.log(A)
 console.log(B)
 
-if(  A < B  ){
-    let resultado = (-A) + (+B)
+if(  a < e  ){
+    let resultado = (-a) + (+e)
     console.log("El resultado es: ")
     console.log(resultado)
 
 }else{
-    if(A > B ){
-    let resultado = A * B
+    if(a > e ){
+    let resultado = a * e
     console.log("El resultado es: ")
     console.log(resultado)
  }
